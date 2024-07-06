6187beac9846ff29ad741dfb9675e9c10bd82025
skysumitkumar <sky3081999@gmail.com>
2024-07-06 14:40:14 +0530

    38_React_basic

---
+++ ALL_React_Classes/React/src/components/React_Basic.js
@@ -0,0 +1,79 @@
+/* ----------------------   38_React_Basics    ------------------------------------- */
+
+// // create a component
+// // link css
+// import './React_Basic.css';
+// // function Item(){
+// //     const day=20;
+// //     return (
+// //         <div>
+// //             <p className="nirma">Nirma</p>
+// //             <span>{day}</span>
+// //         </div>
+// // );
+// // }
+
+
+// // // here we use props
+// // function Item(props){
+// //     const name=props.name;
+// //     const day=props.day;
+// //     const sex=props.sex;
+// //     return (
+// //         <div>
+// //         <p className="nirma">{name}</p>
+// //             <span>{day}</span><br></br>
+// //             <span>{sex}</span>
+// //         </div>
+// // );
+// // }
+
+// // // use response
+// // function Item(props){
+// //     const name=props.name;
+// //     const date=props.day;
+// //     const month=props.month;
+// //     const year=props.year;
+// //     return (
+// //         <div>
+// //         <p className="nirma">{name}</p>
+// //             <span>{date}</span><br></br>
+// //             <span>{month}</span><br></br>
+// //             <span>{year}</span>
+// //         </div>
+// // );
+// // }
+
+// // use response
+// function Item(props){
+//     const name=props.name;
+//     const date=props.day;
+//     const month=props.month;
+//     const year=props.year;
+//     return (
+//         <div>
+//         <p className="nirma">{name}</p>
+//         {props.children}
+//             <span>{date}</span><br></br>
+//             <span>{month}</span><br></br>
+//             <span>{year}</span>
+//         </div>
+// );
+// }
+// // export component to use in App.jsx file
+// export default Item;
+
+/* ----------------------   39_React_Basics    ------------------------------------- */
+
+// create a component
+// link css
+import './React_Basic.css';
+function Item(props){
+    return (
+        <div>
+        hello
+        </div>
+);
+}
+// export component to use in App.jsx file
+export default Item;
