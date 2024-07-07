b0ccb6f4f60c77e33f043e06676cefcf0bd3fabe
skysumitkumar <sky3081999@gmail.com>
2024-07-07 15:13:20 +0530

    25_javascript

---
+++ 25_Javascript.js
@@ -0,0 +1,54 @@
+// console.log("i am linking js file");
+
+// to run js file using terminal :-node 25_Javascript.js
+
+// let a=5;
+// let b=6;
+// console.log(a+b);
+
+// // var:-its scope is global,we can redeclare var variable
+// if(1)
+// {
+//     let a=5;
+//     var b=6;
+// }
+// // it cant't print its scope is end
+// //console.log(a);
+// // its scope is globally so it print 6
+// var b=7;
+// console.log(b);
+
+// const num=12;
+// num=90;
+// console.log(num);
+
+// while making variate always use camel case: sumitKumar
+
+// Primitive Types
+//String,number,boolean,undefined,null
+
+// // dynamic Type
+// let lastName="kumar";
+// console.log(lastName);
+// lastName=3;
+// console.log(lastName);
+
+// Reference Types
+// Objects,Arrays,Function
+// Arrays:-Data structure used to contain a list of items it take all types of data
+
+// Equality Operator
+// Loose equality:-value of both no have same
+// Strict Equality:-value and data type of both no have same
+
+//Ternary Operator(conditional operator)
+//a>b?console.log("a is greater"):console.log("b is greater");
+
+// Logical operator
+//falsey values:-undefined,null,0,false,not a no(NaN),''
+//truthy values:-not a falsey value
+// it print sumit on or case in case of and it print false
+// it called sort curcit
+//false||"sumit"
+console.log("hello");
+
